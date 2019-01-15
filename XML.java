e9f688f1499f23b737828fd5f7850893c0d72d33
IG1GY <thkiwow@gmail.com>
2019-01-15 11:37:52 +0200

    done some changes to xml.java. can now insert transactions. not yet tested..

--- XML.java
+++ XML.java
@@ -1,20 +1,30 @@
 import org.w3c.dom.*;
+
 import javax.xml.parsers.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 //just copied most of it from tutorial's point
 public class XML{
 
     Element root;
+    Document document;
+    File file;
+
     public XML() throws Exception{
 
-        Document document;
         try{
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
-            document = builder.parse(new File("records.xml"));
+            document = builder.parse(file = new File("records.xml"));
         //Document document = builder.parse();
         }catch(Exception e){
 
@@ -34,7 +44,65 @@ public class XML{
         return list;
     }
 
+/*
+<Transaction datetime="">
+    <description>initial value</description>
+    <amount>
+        <currency type="nis" int="50" />
+    </amount>
+</Transaction>
+*/
+
+    public void addTransactions(List<Transaction> transactions){
+
+        NodeList list2 = root.getElementsByTagName("Source");
+        Element source = null;
+
+        for(int i=0; i<list2.getLength() && elem == null; i++)
+            if(list2.item(i).getAttributeNode("value") == transaction.source) elem = (Element) list2.item(i);
+
+        if(elem == null)
+            throw new Exception("no such source: " + source);
+
+        Element Transactions = (Element) elem.getElementsByTagName("Transactions").item(0);
+
+        for(Transaction transaction : transactions){
+
+            Element ntransaction = document.createElement("Transaction");
+            ntransaction.setAttribute("datetime", Long.toString(System.currentTimeMillis()));
+
+            Element description = document.createElement("description");
+            description.appendChild(document.createTextNode(transaction.information));
+
+            Element amount = document.createElement("amount");
+
+            for(Map.Entry<String, Integer> entry : transaction.amount.entrySet()){
+                Element currency = document.createElement("currency");
+
+                currency.setAttribute("type", entry.getKey());
+                currency.setAttrobute("type", entry.getValue());
+                amount.appendChild(currency);
+            }
+
+            ntransaction.appendChild(description);
+            ntransaction.appendChild(amount);
+
+            Transactions.appendChild(ntransaction);
+        }
+        this.save();
+    }
+
+    private void save(){
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(document);
+        StreamResult result = new StreamResult(this.file);
+
+        transformer.transform(source, result);
+    }
     public static void main(String[] args){
+
         try{
             XML xml = new XML();
             System.out.println(xml.getSources());
